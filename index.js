75f35f3f964e0a061b7c27212eb4ffde3d7ade9a
Hernán Peralta <alegodemencia@gmail.com>
2020-08-03 10:11:59 -0300

    Funcionalidad completada
    El proyecto funciona completamente

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const multer = require('multer');
-
 const upload = multer({ dest: './public/uploads/imagenes' });
-
 const exphbs = require('express-handlebars');
 
 const PUERTO = 8080;
@@ -12,33 +10,39 @@ const hbs = exphbs.create();
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
-//app.use(express.static(path.join(__dirname, '/public')));
-//app.use('/static', express.static(path.join(__dirname, 'public')))
-// app.use('/public', express.static('public'))
-// app.use('/public', express.static(`${__dirname}/public`));
-
 app.use(express.static(`${__dirname}/public`));
-// app.use('/ver', express.static(`${__dirname}/public`));
+
 
 app.get('/', (req, res) => {
-  const equipos = JSON.parse(fs.readFileSync('./public/data/equipos.db.json'));
-  res.render('home',{
-    layout: 'layout',
-    equipos: equipos,
+  fs.readFile('./public/data/equipos.db.json', (err, data) => {
+    res.render('home',{
+      layout: 'layout',
+      equipos: JSON.parse(data),
+      titleTag: 'CRUD Clubes',
+      script: '/src/main.js',
+    });
   });
 });
 
 
 app.get('/equipo/:tla/ver', (req, res) => {
   const tla = req.params.tla
-  const equipo = JSON.parse(fs.readFileSync(`./public/data/equipos/${tla}.json`));
-  const vieneDeWeb = equipo.crestUrl.slice(0,4) === 'http';
-  res.render('ver',{
-    layout: 'layout',
-    equipo,
-    vieneDeWeb,
-  });
+  //const vieneDeWeb = equipo.crestUrl.slice(0,4) === 'http';
+  let indiceEquipo = 0;
+    fs.readFile(`./public/data/equipos.db.json`, (err, data) => {
+      for (let i = 0; i < (JSON.parse(data)).length; i++) {
+        if ((JSON.parse(data))[i].tla === tla){
+          indiceEquipo = i;
+        }; 
+      }
+
+      res.render('ver',{
+        layout: 'layout',
+        equipo: JSON.parse(data)[indiceEquipo],
+        //vieneDeWeb,
+        titleTag: JSON.parse(data).shortName,
+      });
+  })
 });
 
 
@@ -50,13 +54,12 @@ app.get('/nuevo', (req, res) => {
 
 
 app.post('/nuevo', upload.single('crestUrl'), (req, res) => {
-  res.render('nuevoequipo',{
-    layout: 'layout',
-    data:{
-      mensaje: "¡Exito!",
-      nombreArchivo: req.file.filename,
-    },
-  });
+  //si no subo ninguna imagen creo el campo file.filename para que no me rompa toda la aplicacion
+  if (req.file === undefined){
+    req.file = {};
+    req.file.filename = "";
+  }
+
   let archivodb = JSON.parse(fs.readFileSync('./public/data/equipos.db.json'));
   const nuevoequipo = {
                       "id": archivodb[archivodb.length - 1].id + 1,
@@ -67,7 +70,7 @@ app.post('/nuevo', upload.single('crestUrl'), (req, res) => {
                       "name": req.body.name,
                       "shortName": req.body.shortName,
                       "tla": req.body.tla,
-                      "crestUrl":req.file.name,
+                      "crestUrl":req.file.filename,
                       "address": req.body.address,
                       "phone": req.body.phone,
                       "website": req.body.website,
@@ -78,31 +81,107 @@ app.post('/nuevo', upload.single('crestUrl'), (req, res) => {
                       "lastUpdated": (new Date).toISOString(),
                     };
 
-  //creo el archivo .json dentro de /equipos y le adoso los arrays para las competiciones y los jugadores
-  let archivoNuevoEquipo = JSON.parse(JSON.stringify(nuevoequipo));
-  archivoNuevoEquipo = Object.assign(archivoNuevoEquipo, {"activeCompetitions":[], "squad": []});
-  fs.writeFileSync(`./public/data/equipos/${nuevoequipo.tla}.json`, JSON.stringify(archivoNuevoEquipo));
-
   archivodb.push(nuevoequipo);
   fs.writeFileSync('./public/data/equipos.db.json', JSON.stringify(archivodb, null, 2));
 
+  res.render('nuevoequipo',{
+    layout: 'layout',
+    data:{
+      mensaje: "¡Exito!",
+      nombreArchivo: req.file.filename,
+    },
+  });
+  
+  //res.redirect("/")
+  //setTimeout(() => res.redirect("/"), 3000);
 });
 
 
 app.get('/equipo/:tla/editar', (req, res) => {
   const tla = req.params.tla
-  const equipo = JSON.parse(fs.readFileSync(`./public/data/equipos/${tla}.json`));
+  const archivodb = JSON.parse(fs.readFileSync(`./public/data/equipos.db.json`));
+  let indiceEquipo = 0;
+  
+  for (let i = 0; i < archivodb.length; i++) {
+    if(archivodb[i].tla === tla){
+      indiceEquipo = i;
+    }
+  }
+  
+  const vieneDeWeb = archivodb[indiceEquipo].crestUrl.slice(0,4) === 'http';
+
   res.render('editar',{
     layout: 'layout',
-    equipo,
+    equipo: archivodb[indiceEquipo],
+    vieneDeWeb,
+    titleTag: archivodb[indiceEquipo].shortName,
+    script: '/src/edit.js',
   });
 });
 
 
-// app.patch('/equipo/:tla/editar', (req, res) => {
+//terminar de implementar esto
+app.post('/equipo/:tla/editar', upload.none(), (req, res) => {
+  const tla = req.params.tla
+  let archivodb = JSON.parse(fs.readFileSync('./public/data/equipos.db.json'));
+  let indiceEquipo = 0;
+  
+  for (let i = 0; i < archivodb.length; i++) {
+    if(archivodb[i].tla === tla){
+      indiceEquipo = i;
+    }
+  }
+  
+  for (key in req.body){
+    archivodb[indiceEquipo][key] = req.body[key]
+  }
 
-// })
+  const vieneDeWeb = archivodb[indiceEquipo].crestUrl.slice(0,4) === 'http';
 
+  fs.writeFileSync('./public/data/equipos.db.json', JSON.stringify(archivodb, null, 2))
+
+  res.render('editar',{
+    layout: 'layout',
+    equipo:archivodb[indiceEquipo],
+    vieneDeWeb,
+    data:{
+      mensaje: "¡Equipo actualizado con éxito!",
+    }
+  });
+
+})
+
+//cambiar a app.get
+app.delete('/equipo/:tla/borrar', (req, res) => {
+  const tla = req.params.tla
+
+  //borrar la entrada donde tla es tla
+  let archivodb = JSON.parse(fs.readFileSync('./public/data/equipos.db.json'));
+
+  let indiceABorrar;
+  let nombreImagen;
+
+  for(key in archivodb){
+    if(archivodb[key].tla === tla){
+      indiceABorrar = key;
+      nombreImagen = archivodb[key].crestUrl;
+      // borro el elemento
+      archivodb.splice(indiceABorrar, 1)
+    }
+  }
+  
+  try{
+    fs.unlinkSync(`./public/uploads/imagenes/${nombreImagen}`, err => {
+      if(err) throw err;
+  })}
+  catch(err){
+    console.log("hubo un error al intentar borrar la imagen", err)
+  }
+
+  fs.writeFileSync('./public/data/equipos.db.json', JSON.stringify(archivodb, null, 2));
+  res.redirect('/');
+
+})
 
 app.listen(8080);
 console.log(`Escuchando el puerto ${PUERTO}`);
